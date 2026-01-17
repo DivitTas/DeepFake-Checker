e4a2382826afebbf1b59f260052fc0fa3925e1d2
DivitTas <divit.nt@gmail.com>
2026-01-18 00:18:51 +0530

    Lay foundations for training loop

--- train.py
+++ train.py
@@ -1,7 +1,17 @@
 from torchvision import models
 import torch.nn as nn
+from torchvison import transforms
 
 model = models.efficientnet_b4(weights = models.EfficientNet_B4_Weights.IMAGENET1K_V1)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.classifier[1] = nn.Linear(model.classifier[1].in_features, 2)
 model = model.to(device)
+
+model.train()
+print("Meow we be training")
+
+epochs = 10
+for epoch in range(epochs):
+    print(f"Epoch {epoch+1}/{epochs} running...")
+    #blah blah training code
+    
